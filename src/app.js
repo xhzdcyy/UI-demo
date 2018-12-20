3fee9df5a9cf06db6062073e3df3576bce529cab
xhzdcyy <912575054@qq.com>
2018-12-20 19:08:48 +0800

    优化input 支持v-model

--- src/app.js
+++ src/app.js
@@ -2,24 +2,25 @@ import Vue from 'vue';
 import Button from './button';
 import Icon from './icon'
 import ButtonGroup from './button-group'
-import Input from  './input'
+import Input from './input'
 import chai from 'chai';
 import spies from 'chai-spies'
 
 Vue.component('g-button', Button);
 Vue.component('g-icon', Icon);
 Vue.component('g-button-group', ButtonGroup)
-Vue.component('g-input',Input)
+Vue.component('g-input', Input)
 
 new Vue({
     el: '#app',
     data: {
         loading1: false,
         loading2: true,
-        loading3: true
+        loading3: true,
+        message: 'hi'
     },
-    methods:{
-        inputChange(e){
+    methods: {
+        inputChange(e) {
             console.log(e.target.value);
         }
     }
