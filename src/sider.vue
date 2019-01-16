e9af61b3781cae3d441c302302f7555e37b3a5d7
xhzdcyy <912575054@qq.com>
2019-01-16 16:07:53 +0800

    简单完成布局组件

---
+++ src/sider.vue
@@ -0,0 +1,38 @@
+<template>
+    <transition name="fade">
+        <div class="sider" v-if="visible">
+            <slot></slot>
+            <button @click="visible=false">close</button>
+        </div>
+    </transition>
+</template>
+<script>
+    export default {
+        name: 'GuluSider',
+        data() {
+            return {
+                visible: true
+            }
+        },
+        methods:{
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    .sider {
+        position: relative;
+        > button {
+            position: absolute;
+            top: 0;
+            right: 0;
+        }
+    ;
+
+    }
+
+    .fade-enter-active, .fade-leave-active {
+        transition: all .5s;
+    }
+
+
+</style>
