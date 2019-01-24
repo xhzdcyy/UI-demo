35adddcfb65de4a2db70a31ec389fdbe610f3dbf
xhzdcyy <912575054@qq.com>
2019-01-24 23:00:19 +0800

    添加tabs基础测试

---
+++ test/tabs.test.js
@@ -0,0 +1,13 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Tabs from '../src/tabs'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Tabs', () => {
+    it('存在.', () => {
+        expect(Tabs).to.be.ok
+    })
+
+});
