006ab86191cf055c1445568cc9fe0ec896d2cbb9
xhzdcyy <912575054@qq.com>
2019-01-28 16:43:32 +0800

    popover基本功能实现

---
+++ src/popover.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="popover" @click.stop="xxx">
+        <div class="content-wrapper" v-if="visible" ref="contentWrapper" @click.stop>
+            <slot name="content" ></slot>
+        </div>
+        <span ref="triggerWrapper">
+            <slot></slot>
+        </span>
+
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'GuluPopover',
+        data() {
+            return {
+                visible: false
+            }
+        },
+        methods: {
+            xxx() {
+                console.log(1);
+                this.visible = !this.visible;
+                if(this.visible){
+                    this.$nextTick(()=>{
+                        document.body.appendChild(this.$refs.contentWrapper);
+                        let {width,height,top,left} = this.$refs.triggerWrapper.getBoundingClientRect();
+                        this.$refs.contentWrapper.style.left = left+'px';
+                        this.$refs.contentWrapper.style.top = top+'px';
+                        let eventHandler =()=>{
+                            this.visible = false;
+                            document.removeEventListener('click',eventHandler)
+                        };
+                        document.addEventListener('click',eventHandler)
+                    })
+                }
+            }
+        },
+        mounted() {
+        }
+    }
+</script>
+
+<style lang="scss" >
+    .popover {
+        display: inline-block;
+        vertical-align: top;
+        position: relative;
+        color: blue;
+
+    }
+    .content-wrapper{
+        position: absolute;
+        /*bottom: 100%;*/
+        /*left: 0;*/
+        /*color: red;*/
+        border: 1px solid red;
+        box-shadow: 0 0 3px rgba(0,0,0,0.5);
+        transform:translateY(-100%);
+
+    }
+</style>
