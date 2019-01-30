cbf3ca93cae936648299834a2a04b1e02f7dfbf9
xhzdcyy <912575054@qq.com>
2019-01-30 15:23:44 +0800

    添加single选项

--- src/collapse.vue
+++ src/collapse.vue
@@ -6,14 +6,36 @@
 </template>
 
 <script>
+    import Vue from 'vue'
+
     export default {
-        name: 'GuluCollapse'
+        name: 'GuluCollapse',
+        props:{
+            single:{
+                type:Boolean,
+                default:false
+            }
+        },
+        data() {
+            return {
+                eventBus: new Vue
+            }
+        },
+
+        provide() {
+            if(this.single){
+                return {
+                    eventBus:this.eventBus
+                }
+            }
+
+        }
     }
 </script>
 
 <style lang="scss" scoped>
-    $grey:#ddd;
-    $border-radius:4px;
+    $grey: #ddd;
+    $border-radius: 4px;
     .collapse {
         border: 1px solid $grey;
         border-radius: $border-radius;
